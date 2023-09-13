a662f5a3673e6d3490c18f667077f0c8c523a514
oaepe-cs <141412247+oaepe-cs@users.noreply.github.com>
2023-09-13 22:30:43 +0800

    Revert "Revert "tks""
    This reverts commit 3ca2b25cb1c8f26c4463edfcda6f5871977308eb.

---
+++ Week4/Week4/BreakKeywords.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Week4
+{
+    class BreakKeywords
+    {
+        //Instance members
+        string name;
+        int number;
+        bool tfMale;
+
+        //static members
+        static decimal totalResult;
+
+        //properties
+        string FatherName { get; set; }
+
+        //Constructor
+        public BreakKeywords()
+        {
+
+        }
+
+        public BreakKeywords(string _Name)
+        {
+            name = _Name;
+        }
+
+        public BreakKeywords(int _Number)
+        {
+            number = _Number;
+        }
+
+        public BreakKeywords(string _Name, int _Number)
+        {
+            name = _Name;
+            number = _Number;
+        }
+
+        static void BreakKeywordsMain()
+        { 
+            Console.WriteLine("Start Break Statements");
+            //break
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 3)
+                if (i == 5) break;
+                Console.WriteLine("Break Value: " + i);
+            }
+
+        
+            Console.WriteLine("Start continue Statements");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("inside loop");
+                if (i == 5) continue;
+                Console.WriteLine("Continue Value: " + i);
+            }
+            
+            //goto:
+            Console.WriteLine("goto test");
+        repeat:
+            Console.WriteLine("Enter a number less than 10");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num >= 10)
+            {
+                // transfers control to repeat
+                goto repeat;
+            }
+            Console.WriteLine(num + " is less than 10");
+
+            Console.WriteLine("End of statements");
+            Console.ReadLine();
+        }
+    }
+}
