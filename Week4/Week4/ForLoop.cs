9e2ab70894513e1217f5fba543790d4057c8f04a
oaepe-cs <141412247+oaepe-cs@users.noreply.github.com>
2023-09-13 22:27:47 +0800

    tks

--- Week4/Week4/ForLoop.cs
+++ Week4/Week4/ForLoop.cs
@@ -7,7 +7,7 @@ namespace Week4
 {
     class ForLoop
     {
-        static void Main()
+        static void ForLoopMain()
         {
             //for loop
             //for(initialization; condition; increment/decrement)
@@ -22,13 +22,110 @@ namespace Week4
             }
             
             //Decrement
-            for (i = 10; (i >= 0); i = i - 1)
+            for (i = 10; (i >= 0); i = i - 3)
             {
-                Console.WriteLine(i + 100);
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+            for (int x = 0; x < 10; x = x + 2)
+            {
+                Console.WriteLine("X " + x);
+            }
+
+            int[] arr = new int[11];
+            arr[0] = 100;
+            arr[1] = 105;
+            arr[2] = 110;
+            arr[3] = 120;
+            arr[4] = 125;
+            arr[5] = 130;
+            arr[6] = 135;
+            arr[7] = 140;
+            arr[8] = 145;
+            arr[9] = 150;
+
+            int totalSum = 0;
+            for (int c = 0; c < arr.Length; c++)
+            {
+                if (c % 2 == 0)
+                {
+                    totalSum = totalSum + arr[c];
+                    Console.WriteLine("Arr[" + c + "]: " + arr[c]);
+                }                
+            }
+            Console.WriteLine("Total Sum: " + totalSum);
+
+
+            //Factorial
+            //3! => 3*2*1= 6
+            //5! => 5*4*3*2*1 = 120
+            Console.WriteLine("Enter Number fro Factorial Calculation");
+            int num = int.Parse(Console.ReadLine());
+            int result = 1;
+            for (int j = 1; j <= num; j++)
+            {
+                result = result * j;
+            }
+            Console.WriteLine("Result :" + result);
+
+            //Nested Loop
+            for (int x = 1; x <= 5; x++)
+            {
+                Console.WriteLine("x :" + x);
+                for (int y = 1; y <= 10; y++)
+                {
+                    Console.WriteLine("..: " + y);
+                }
+            }
+
+            int[] temArr = arr;
+            foreach (int y in temArr)
+            {
+                Console.WriteLine(y);
+            }
+            string[] strArr = new string[2];
+            strArr[0] = "Weekdays";
+            strArr[1] = "Weekend";
+
+            string[] strArray = new string[]
+            {
+                "Sunday", 
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday"
+            };
+
+            //For
+            for (int weekDays = 0; weekDays < strArr.Length; weekDays++)
+            {
+                Console.WriteLine("Weeks: " + strArr[weekDays]);
+                for (int days = 0; days < strArray.Length; days++)
+                {
+                    Console.WriteLine("Days: " + strArray[days]);
+                }
+            }
+            
+            //ForEach
+            foreach (string week in strArr)
+            {
+                string outPut = string.Format("It comes from Foreach {0}", week);
+                Console.WriteLine(outPut);
+            }
+
+            foreach (var item in strArray)
+            {
+                string output = string.Format("Foreach {0}", item);
+                Console.WriteLine(output);
             }
 
             Console.WriteLine("End of Foor looop.");
             Console.ReadLine();
         }
+
+        
     }
 }
