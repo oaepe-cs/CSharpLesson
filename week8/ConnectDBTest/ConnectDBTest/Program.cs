8754ac42dc7be16e5438b6cd999705d6d24530e4
oaepe-cs <141412247+oaepe-cs@users.noreply.github.com>
2023-10-18 22:09:21 +0800

    Week 8

---
+++ week8/ConnectDBTest/ConnectDBTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConnectDBTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionStringWithWindowAuth = "Server=CS;Database=DBSpring;IntegratedSecurity=True;";
+            string connectionStringWithServerAuth = "Server=CS;Database=DBSpring;User Id=sa;Password=123456789cs";
+            using (SqlConnection conn = new SqlConnection(connectionStringWithServerAuth))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("SELECT PersonID, Name, City FROM tbl_Persons", conn))
+                {
+                    using(SqlDataReader rd = cmd.ExecuteReader())
+                    {
+                        while (rd.Read())
+                        {
+                            int PersonID = rd.GetInt32(0);
+                            string Name = rd.GetString(1);
+                            string City = rd["City"].ToString();
+                            Console.WriteLine("PersonID: " + PersonID + ", Name: " + Name + ", City: " + City);
+                        }
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
